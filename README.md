96e57b5c60b05639f0937d2893f2262e1fe8de63
张深态 <78351684+MRNOBODY-ZST@users.noreply.github.com>
2022-05-05 13:56:08 +0800

    plugins modify

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
+<!--
+ * @Author: 张深态 78351684+MRNOBODY-ZST@users.noreply.github.com
+ * @Date: 2022-05-05 10:12:06
+ * @LastEditors: 张深态 78351684+MRNOBODY-ZST@users.noreply.github.com
+ * @LastEditTime: 2022-05-05 10:34:15
+ * @FilePath: \MirrorServerSync\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 # Mirror Server Sync
+
 A Simple MCDR Plugin To Sync Map Files Of Minecraft On Different Servers
 
 ## 一、使用前配置
 
-​		本插件适用于Windows系统与Linux系统
+本插件适用于Windows系统与Linux系统
 
-​		在使用本插件前，你需要在你的服务器上安装Rsync
+​在使用本插件前，你需要在你的服务器上安装Rsync
 
-​		Linux安装方法：`yum -y install rsync`
+​Linux安装方法：`yum -y install rsync`
 
-​		Windows安装方法：[cwRsync - Rsync for Windows | itefix.net](https://www.itefix.net/cwrsync)下载文件后安装Exe文件
+​Windows安装方法：[cwRsync - Rsync for Windows | itefix.net](https://www.itefix.net/cwrsync)下载文件后安装Exe文件
 
-​		一般服务器上默认会安装Rsync，为了保险，手动安装一遍。
+一般服务器上默认会安装Rsync，为了保险，手动安装一遍。
 
-​		请确保您的服务器放行了所有端口以确保Rsync能正常工作
+请确保您的服务器放行了所有端口以确保Rsync能正常工作
 
-​		打开终端，执行`ssh-keygen`。无需输入密码，无脑确认即可。再执行`ssh-copy-id {Main Server IP/Mirror Server IP}`（IP地址取决于你是在Main Server还是再Mirror Server，Main就写Mirror，Mirror写Main）之后确认Fingerprint，输入yes后输入对方服务器的密码，Enter。
+打开终端，执行`ssh-keygen`。无需输入密码，无脑确认即可。再执行`ssh-copy-id {Main Server IP/Mirror Server IP}`（IP地址取决于你是在Main Server还是再Mirror Server，Main就写Mirror，Mirror写Main）之后确认Fingerprint，输入yes后输入对方服务器的密码，Enter。
 
-​		之后配置config.json。把config.json拖入MCDR的config文件夹中，修改以下内容：
+之后配置config.json。把config.json拖入MCDR的config文件夹中，修改以下内容：
 
 ```json
 {
@@ -42,7 +51,6 @@ A Simple MCDR Plugin To Sync Map Files Of Minecraft On Different Servers
 }
 ```
 
-​		修改完后就可以启动MCDR
+修改完后就可以启动MCDR
 
 ## 二、指令说明
-
--- plugins/mirror_server_sync.py
+++ plugins/mirror_server_sync.py
@@ -1,18 +1,34 @@
-from multiprocessing.connection import wait
+'''
+Author: 张深态 78351684+MRNOBODY-ZST@users.noreply.github.com
+Date: 2022-05-05 10:12:06
+LastEditors: 张深态 78351684+MRNOBODY-ZST@users.noreply.github.com
+LastEditTime: 2022-05-05 13:32:37
+FilePath: \MirrorServerSync\plugin\entry.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from http import server
+import os
 import subprocess
 import json
-
-from mcdreforged.api.types import PluginServerInterface, PlayerCommandSource
+from mcdreforged.api.types import PluginServerInterface, PlayerCommandSource, CommandSource
 from mcdreforged.api.command import Literal
 
-json_file_pos = "../Mirror_Server_Sync.json"
+def get_json_location():
+    server = PluginServerInterface()
+    meta = server.get_all_metadata()
+    print(meta.values())
 
-with open(json_file_pos) as json_file:
-    data = json.load(json_file)
-    main_server_dir = data["main_server_dir"]
-    mirror_server_dir = data["mirror_server_dir"]
-    main_server_ip = data["main_server_ip"]
-    world_name = data["world_name"]
+def open_json(file_name,config_location):
+    global main_server_dir,main_server_ip,mirror_server_dir,world_name
+    try:
+        with open('{1}/{0}'.format(file_name,config_location),'r') as json_file:
+            data = json.load(json_file)
+            main_server_dir = data["main_server_dir"]
+            mirror_server_dir = data["mirror_server_dir"]
+            main_server_ip = data["main_server_ip"]
+            world_name = data["world_name"] 
+    except:
+        pass
 
 def sync_world(main_server_dir, mirror_server_dir, main_server_ip, world_name):
     command_rsync = "rsync -avPz --progress {0}:{1}+{3} {2}".format(main_server_ip, main_server_dir, mirror_server_dir, world_name)
@@ -29,13 +45,13 @@ def show_help(src: CommandSource):
     # TODO 在src中显示上面的注释信息
     return None
 
-def show_permission_fail(src: CommandSource):
+def show_permission_fail(src: PlayerCommandSource):
     # TODO 在src中显示权限不足信息
     return None
 
 # 确认权限等级
-def permission_check(src: CommandSource):
-    return src.has_permission(2):
+def permission_check(src: PlayerCommandSource):
+    return src.has_permission(2)
 
 # 注册指令树
 def register_commands(server: PluginServerInterface):
@@ -60,3 +76,5 @@ def register_commands(server: PluginServerInterface):
 
 def on_load(server: PluginServerInterface, old):
     register_commands(server)
+
+get_json_location()
