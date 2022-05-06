92e9e75620091d6d65848f1a0656c3d29eb0bc71
张深态 <78351684+MRNOBODY-ZST@users.noreply.github.com>
2022-05-06 20:49:26 +0800

    Update mirror_server_sync.py

--- plugins/mirror_server_sync.py
+++ plugins/mirror_server_sync.py
@@ -1,30 +1,4 @@
-'''
- ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
- │Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│  ┌┐    ┌┐    ┌┐
- └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘  └┘    └┘    └┘
- ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
- │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│ │N L│ / │ * │ - │
- ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
- │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│ │ 7 │ 8 │ 9 │   │
- ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
- │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │               │ 4 │ 5 │ 6 │   │
- ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
- │ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │     │ 1 │ 2 │ 3 │   │
- ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
- │ Ctrl│    │Alt │         Space         │ Alt│    │    │Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
- └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
-
-Author: 张深态 78351684+MRNOBODY-ZST@users.noreply.github.com
-Date: 2022-05-05 10:12:06
-LastEditors: 张深态 78351684+MRNOBODY-ZST@users.noreply.github.com
-LastEditTime: 2022-05-05 16:50:48
-FilePath: \MirrorServerSync\plugins\mirror_server_sync.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
-
-'''
-'''
-
+from cProfile import run
 import os
 import subprocess
 import json
@@ -36,6 +10,17 @@ def get_json_location():
     meta = server.get_all_metadata()
     print(meta.values())
 
+
+def qb_back():
+    pass
+
+def qb_list():
+    pass
+
+def rcon_connect():
+    pass
+
+
 def open_json(server: PluginServerInterface):
     global main_server_dir,main_server_ip,mirror_server_dir,world_name
     try:
@@ -45,6 +30,7 @@ def open_json(server: PluginServerInterface):
             mirror_server_dir = data["mirror_server_dir"]
             main_server_ip = data["main_server_ip"]
             world_name = data["world_name"] 
+            return main_server_dir,main_server_ip,mirror_server_dir,world_name
     except:
         with open(os.path.join(server.get_data_folder(), 'mirror_server_sync.json'), 'w') as json_file:
             json_file.write('''
@@ -73,7 +59,7 @@ def open_json(server: PluginServerInterface):
         
 
 def sync_world(main_server_dir, mirror_server_dir, main_server_ip, world_name):
-    command_rsync = "rsync -avPz --progress {0}:{1}+{3} {2}".format(main_server_ip, main_server_dir, mirror_server_dir, world_name)
+    command_rsync = "rsync -avPz --progress {0}:{1}/{3} {2}".format(main_server_ip, main_server_dir, mirror_server_dir, world_name)
     cmd_content = subprocess.run(command_rsync,shell="True")
     print(cmd_content)
 
@@ -98,23 +84,28 @@ def permission_check(src: CommandSource):
 # 注册指令树
 def register_commands(server: PluginServerInterface):
     server.register_command(
-        Literal('!!msync')
+        Literal('!!msync').requires(lambda src: src.has_permission(4)).runs(show_help())
         .then(
-            Literal("peek")
+            Literal({"peek","-p","p"})
+            .requires(permission_check, show_permission_fail)
+            .runs(qb_list())
+        )
+        .then(
+            Literal({"sync","s","-s"})
+            .requires(permission_check, show_permission_fail)
+            .runs(sync_world(open_json()))
+        )
+        .then(
+            Literal({"recover","r","-r"})
             .requires(permission_check, show_permission_fail)
             .runs()
         )
         .then(
-            Literal("sync")
+            Literal({"help","h","-h"})
             .requires(permission_check, show_permission_fail)
             .runs()
         )
-        .then(
-            Literal("recover")
-            .requires(permission_check, show_permission_fail)
-            .runs()
-        )
-    ).runs(show_help)
+    )
 
 def on_load(server: PluginServerInterface, old):
 #    register_commands(server)
