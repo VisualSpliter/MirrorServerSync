1d73306dcbab0b764005b5232c2640b1c51945cb
张深态 <78351684+MRNOBODY-ZST@users.noreply.github.com>
2022-05-09 16:04:31 +0800

    Adding Feature
    Adding Minecraft Rcon Feature
    Fix bugs

--- plugins/mirror_server_sync.py
+++ plugins/mirror_server_sync.py
@@ -3,7 +3,7 @@ import subprocess
 import json
 import rcon
 import mcrcon
-from mcdreforged.api.types import PluginServerInterface, PlayerCommandSource, CommandSource
+from mcdreforged.api.types import PluginServerInterface, PlayerCommandSource, CommandSource, ServerInterface , ServerInformation
 from mcdreforged.api.command import Literal
 
 def get_json_location(server: PluginServerInterface):
@@ -18,12 +18,15 @@ def qb_back(server: PluginServerInterface, src: CommandSource):
 def qb_list(server: PluginServerInterface, src: CommandSource):
     pass
 
-def rcon_connect(server: PluginServerInterface, src: CommandSource):
-    pass
+def rcon_connect(si:ServerInterface):
+    minecraft_rcon = mcrcon.MCRcon(main_server_ip,rcon_password,int(rcon_port))
+    minecraft_rcon.connect()
+    si.logger.info("RCON Connected IP:{0} Port:{1}".format(main_server_ip,rcon_port))
 
 
 def open_json(server: PluginServerInterface):
     global main_server_dir,main_server_ip,mirror_server_dir,world_name
+    global rcon_port,rcon_password
     try:
         with open(os.path.join(server.get_data_folder(), 'mirror_server_sync.json'), 'r') as json_file:
             data = json.load(json_file)
@@ -31,7 +34,8 @@ def open_json(server: PluginServerInterface):
             mirror_server_dir = data["mirror_server_dir"]
             main_server_ip = data["main_server_ip"]
             world_name = data["world_name"] 
-            return main_server_dir,main_server_ip,mirror_server_dir,world_name
+            rcon_port = data["rcon_port"]
+            rcon_password = data["rcon_password"]
     except:
         with open(os.path.join(server.get_data_folder(), 'mirror_server_sync.json'), 'w') as json_file:
             json_file.write('''
@@ -40,8 +44,11 @@ def open_json(server: PluginServerInterface):
     "world_name": "",
     "main_server_dir": "/root/my_mcdr_server/server",
     "mirror_server_dir": "/root/my_mcdr_server/server",
-}
-            ''')
+    "rcon_password": "",
+    "rcon_port": ""
+}''')
+        open_json(server)
+
             
         
 
@@ -60,8 +67,8 @@ def show_help(src: CommandSource):
     # TODO 在src中显示上面的注释信息
     return None
 
-def do_nothing():
-    pass
+def do_nothing(server: PluginServerInterface):
+    server.load_config_simple()
 
 def show_permission_fail(src: CommandSource):
     # TODO 在src中显示权限不足信息
@@ -72,31 +79,34 @@ def permission_check(src: CommandSource):
     return src.has_permission(2)
 
 # 注册指令树
-def register_commands(server: PluginServerInterface,src: CommandSource):
+def register_commands(server: PluginServerInterface, src: CommandSource):
     server.register_command(
-        Literal('!!msync').requires(src.has_permission(4)).runs(show_help())
+        Literal('!!msync').requires(permission_check,show_permission_fail).runs(show_help)
         .then(
             Literal({"peek","-p","p"})
             .requires(permission_check, show_permission_fail)
-            .runs(qb_list())
+            .runs(do_nothing)
         )
         .then(
             Literal({"sync","s","-s"})
             .requires(permission_check, show_permission_fail)
-            .runs(sync_world(main_server_dir=main_server_dir,main_server_ip=main_server_ip,world_name=world_name,mirror_server_dir=mirror_server_dir)) #啊吧啊吧皮这一下很开心
+            .runs(sync_world) #啊吧啊吧皮这一下很开心
         )
         .then(
             Literal({"recover","r","-r"})
             .requires(permission_check, show_permission_fail)
-            .runs(qb_back())
+            .runs(do_nothing)
         )
         .then(
             Literal({"help","h","-h"})
             .requires(permission_check, show_permission_fail)
-            .runs(show_help())
+            .runs(do_nothing)
         )
     )
 
 def on_load(server: PluginServerInterface, old):
+    src = CommandSource
+    si = ServerInterface
     open_json(server)
-    register_commands(server)
+    rcon_connect(si)
+    register_commands(server,src)
